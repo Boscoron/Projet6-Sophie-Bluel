c11e4a71a304e8959389a2ac75d746a755f67fcf
Boscoron <boukrim.yahya19@gmail.com>
2025-01-25 11:20:25 +0100

    update : mise en place de la modale(début)

--- FrontEnd/fetch.js
+++ FrontEnd/fetch.js
@@ -101,11 +101,61 @@ function activeAdmin() {
     // activation du visuel editMod
     const editMod = document.createElement("div");
     editMod.className = "editMod";
-    editMod.innerHTML = '<p><i class="fa-regular fa-pen-to-square"></i> Mode édition activé</p>';
+    editMod.innerHTML = '<p><a href="#modal1" class="js-modal"><i class="fa-regular fa-pen-to-square"></i> Mode édition activé</a></p>';
     document.body.prepend(editMod);
   }
 }
 
 activeAdmin();
+
+let modal = null
+
+const openModal = function (e) {
+  e.preventDefault()
+
+  const target = document.querySelector(e.target.getAttribute("href"));
+
+  target.style.display = null;
+  target.removeAttribute("aria-hidden");
+  target.setAttribute("aria-modal", "true");
+  modal = target;
+  modal.addEventListener('click', closeModal);
+  modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+  modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+}
+
+const closeModal = function (e) {
+  if (modal === null) return
+  e.preventDefault();
+
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", "true");
+  modal.removeAttribute("aria-modal");
+  modal.removeEventListener('click', closeModal);
+  modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+  modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+  modal = null;
+}
+
+const stopPropagation = function (e) {
+  e.stopPropagation()
+}
+
+const focusInModal = function (e) {
+  e.preventDefault()
+}
+
+document.querySelectorAll(".js-modal").forEach(a => {
+  a.addEventListener("click", openModal);
+});
+
+window.addEventListener('keydown', function (e) {
+  if (e.key === "Escape" || e.key === "Esc") {
+      closeModal(e)
+  }
+  if (e.key === "Tab" && modal !== null) {
+    focusInModal(e)
+  }
+})
 
 });
